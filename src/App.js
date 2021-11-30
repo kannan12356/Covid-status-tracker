ed18b51aa3f54188a730d1f87f70b9744f4828f9
Kannan <kannanaa21@gmail.com>
2021-11-30 16:35:57 +0530

    initialize app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
+import Header from './Components/Header';
+import India from './Components/India';
+import World from './Components/World';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends Component{
+
+  render(){
+    return(      
+      <Router>
+        <div>
+          <Header/>
+          <div className="container-fluid">
+            <Routes>
+              <Route path="/" element={<India/>}/>
+              <Route path="/India" element={<India/>}/>
+              <Route path="/World" element={<World/>}/>
+            </Routes>          
+          </div>
+        </div>
+      </Router>
+    )
+  }
 }
-
 export default App;
